3ac1e9af8a202a37d1ca4aeba852e640c3593cb9
noamman27 <153654176+noamman27@users.noreply.github.com>
2026-06-03 18:06:14 +0300

    fixed gettype

--- lib/utils.c
+++ lib/utils.c
@@ -4,16 +4,16 @@
 int gettype(char *s, char *t){
     int i = 0;
     while (*s){
-        if(i <= 8 && strcmp(*s, R_commands[i])){
-            *t = R_commands[i];
+        if(i <= 8 && strcmp(*s, rCommands[i].name)){
+            *t = rCommands[i].name;
             return 1;
         }
-        if(i <= 4 && strcmp(*s, I_commands[i])){
-            *t = I_commands[i];
+        if(i <= 4 && strcmp(*s, rCommands[i].name)){
+            *t = rCommands[i].name;
             return 1;
         }
-        if(i <= 16 && strcmp(*s, J_commands[i])){
-            *t = J_commands[i];
+        if(i <= 16 && strcmp(*s, jCommands[i].name)){
+            *t = jCommands[i].name;
             return 1;
         }
     }
