09f4bc1e4356bac16276e8beb02e4d13138020c0
noamman27 <153654176+noamman27@users.noreply.github.com>
2026-06-03 18:18:43 +0300

    gettype is better

--- lib/utils.c
+++ lib/utils.c
@@ -5,15 +5,15 @@ int gettype(char *s, char *t){
     int i = 0;
     while (*s){
         if(i <= 8 && strcmp(*s, rCommands[i].name)){
-            *t = rCommands[i].name;
+            *t = 'r';
             return 1;
         }
-        if(i <= 4 && strcmp(*s, rCommands[i].name)){
-            *t = rCommands[i].name;
+        if(i <= 4 && strcmp(*s, iCommands[i].name)){
+            *t = 'i';
             return 1;
         }
         if(i <= 16 && strcmp(*s, jCommands[i].name)){
-            *t = jCommands[i].name;
+            *t = 'j';
             return 1;
         }
     }
