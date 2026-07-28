682cb08cca7aebaa9dbf03e7cf04801337fb2357
Noam Fichman <153654176+noamman27@users.noreply.github.com>
2026-07-28 10:16:34 +0300

    made sure assembler only accepts .as file and write 3 tests

--- main/main.c
+++ main/main.c
@@ -5,28 +5,36 @@
 
 int main(int argc, char *argv[]){
     int i, j = 0;
-    char name[MAXLINE];
+    char name[MAXLINE], *tmp;
     FILE *original, *pre_assembled;
     if(argc < 2){ /*make sure we were given at least one file*/
         fprintf(stderr, "error: not enough given params\n");
         return 1;
     }
     for(i=1; i<argc; i++){
+        tmp = argv[i];
         while(*argv[i] != '.'){
-            name[j++] = *argv[i];
-            argv[i]++;
+            name[j++] = *tmp;
+            tmp++;
         }
+        if(strcmp(tmp, "as")){ /*check the file extension to ensuer we were given a .as file*/
+            err("error: the program must be given a file ending with .as");
+            continue;
+        }
+        name[j - 1] = '\0';/*close the string without including the .*/
         original = fopen(argv[i], "r"); /*open the input and output files*/
-        pre_assembled = fopen(strcat(name, ".am"), "rw");
+        strcat(name, ".am"); /*add .am to the name*/
+        pre_assembled = fopen(name, "rw"); /*open the file as rw since we need to read and write from it*/
+        name[j-1] = '\0'; /*remove the .am since we need the basename for assembly*/
         if(original == NULL || pre_assembled == NULL){ /*check if we can open them*/
             fprintf(stderr, "error: failed to open input or output file\n"); /*if not we print and error to stderr*/
-            return 1;
+            continue;
         }
         if(!pre_assemble(original, pre_assembled, name)){ /*call pre assembler and make sure no errors were given*/
-            return 1; /* if errors were given we just return 1 since pre assembler prints errors*/
+            continue; /* if errors were given we just continue to next file since pre assembler prints errors*/
         }
         /*if we got to this point it means the assembly didnt encounter any errors, since pre assembler calls first pass which calls second pass*/
-        fclose(original); /*close the files*/
+        fclose(original); /*so we close the files*/
         fclose(pre_assembled);
     }
     j = 0;
